3edf14d4550a3b066b8068a3df963898d0d5033a
shubham bajaj <shmbajaj32@gmail.com>
2024-02-22 08:52:57 +0530

    session 😦🤷

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,38 @@ const express = require("express");
 const layouts = require("express-ejs-layouts");
 const path = require("path");
 const router = require("./src/config/routes");
+const expressSession = require("express-session");
+const passport = require("passport");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const prisma = require("./src/db/helpers/init");
 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = "0.0.0.0";
+const sessionStore = new PrismaSessionStore(prisma, {
+  checkPeriod: 2 * 60 * 1000, //ms
+  dbRecordIdIsSessionId: true,
+  dbRecordIdFunction: undefined,
+});
 
 app.use(layouts);
 app.use(express.static(__dirname + "/public"));
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
+
+//check if sessionStore.sync() method require to replicate as
+//sessionStore.sync() of SequelizeStore
+app.use(passport.authenticate("session"));
+
 app.set("views", path.join(__dirname, "/src/views"));
 app.set("layout", "layouts/application");
 app.set("view engine", "ejs");
